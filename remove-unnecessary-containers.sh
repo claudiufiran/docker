4c2dc9d46505534464663f2a94c1b21f3e011047
EC2 Default User <ec2-user@ip-172-31-16-137.eu-central-1.compute.internal>
2016-12-29 19:05:34 +0000

    plus

--- remove-unnecessary-containers.sh
+++ remove-unnecessary-containers.sh
@@ -1,3 +1,5 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+docker volume rm $(docker volume ls -qf dangling=true)
+
